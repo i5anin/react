eb3f37fc0533ea4740d7d49d4d1b5c4da9b8a16d
PizZzA <sergey.isanin@gmail.com>
2025-04-08 22:54:32 +0300

    refactor(core): App переписан без JSX
    - компонент полностью переведён на использование React.createElement;
    - добавлены комментарии для обозначения декларативного и императивного стиля.

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { createElement, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+// декларативный стиль
+export function CurrentYear() {
+    const year = new Date().getFullYear();
+    return createElement('span', null, year);
+}
+
+// декларативный стиль
+function App() {
+    const [count, setCount] = useState(0);
+
+    // императивный стиль
+    return createElement(
+        React.Fragment,
+        null,
+
+        createElement(
+            'div',
+            null,
+            createElement(
+                'a',
+                { href: 'https://vite.dev', target: '_blank' },
+                createElement('img', {
+                    src: viteLogo,
+                    className: 'logo',
+                    alt: 'Vite logo',
+                })
+            ),
+            createElement(
+                'a',
+                { href: 'https://react.dev', target: '_blank' },
+                createElement('img', {
+                    src: reactLogo,
+                    className: 'logo react',
+                    alt: 'React logo',
+                })
+            )
+        ),
+
+        createElement('h1', null, 'Vite + React'),
+
+        createElement(
+            'div',
+            { className: 'card' },
+            createElement(
+                'button',
+                { onClick: () => setCount((count) => count + 1) },
+                `count is ${count}`
+            ),
+            createElement(
+                'p',
+                null,
+                'Edit ',
+                createElement('code', null, 'src/App.js'),
+                ' and save to test HMR'
+            )
+        ),
+
+        createElement(
+            'p',
+            { className: 'read-the-docs' },
+            'Click on the Vite and React logos to learn more'
+        ),
+
+        createElement(
+            'footer',
+            null,
+            createElement(
+                'p',
+                null,
+                '© ',
+                createElement(CurrentYear, null),
+                ' Все права защищены'
+            )
+        )
+    );
+}
+
+export default App;
